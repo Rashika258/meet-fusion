1a636081f22924f2b95e34c7ac10a935824a9a82
Rashika258 <rashikasuresh5758@gmail.com>
2024-05-17 07:21:29 +0530

    Meeting Room changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Loader from "@/components/Loader";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
@@ -9,6 +11,21 @@ const MeetingPage = () => {
   const { isLoaded, user } = useUser();
   const { call, isCallLoading } = useGetCallById(id);
   const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if(!isLoaded || isCallLoading) {
+    return <Loader />
+  }
+
+  if(!call) {
+    return (
+        <p className="text-center text-3xl font-bold text-white">
+            Call not found
+        </p>
+    )
+  }
+
+  const notAllowed = call.type === 'invalid' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+  
 
   return <main className="h-screen w-full"></main>;
 };
