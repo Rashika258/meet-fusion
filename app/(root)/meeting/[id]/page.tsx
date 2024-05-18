09a817d20081be0456c3fa6cf47dd2b9e5d5cb5d
Rashika258 <rashikasuresh5758@gmail.com>
2024-05-18 13:13:00 +0530

    Page chnages

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
 
@@ -12,23 +15,37 @@ const MeetingPage = () => {
   const { call, isCallLoading } = useGetCallById(id);
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
-  if(!isLoaded || isCallLoading) {
-    return <Loader />
+  if (!isLoaded || isCallLoading) {
+    return <Loader />;
   }
 
-  if(!call) {
+  if (!call) {
     return (
-        <p className="text-center text-3xl font-bold text-white">
-            Call not found
-        </p>
-    )
+      <p className="text-center text-3xl font-bold text-white">
+        Call not found
+      </p>
+    );
   }
 
-  const notAllowed = call.type === 'invalid' && (!user || !call.state.members.find((m) => m.user.id === user.id));
-  
+  const notAllowed =
+    call.type === "invalid" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
 
-  return <main className="h-screen w-full"></main>;
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+        {isSetupComplete ? (
+          <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+        ) : (
+          <MeetingRoom />
+        )}
+
+        </StreamTheme>
+
+      </StreamCall>
+    </main>
+  );
 };
 
 export default MeetingPage;
-
